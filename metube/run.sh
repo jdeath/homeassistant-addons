e70c788f2545e901f3514d9c54f8d79c3fa7f55d
jdeath <17914369+jdeath@users.noreply.github.com>
2024-02-10 07:19:04 -0500

    Update run.sh

--- metube/run.sh
+++ metube/run.sh
@@ -3,12 +3,9 @@ set -e
 
 
 CONFIG_PATH=/data/options.json
-cat /data/options.json
 DELETE_FILE_ON_TRASHCAN=false
 DELETE_FILE_ON_TRASHCAN=$(jq --raw-output '.DELETE_FILE_ON_TRASHCAN' $CONFIG_PATH)
 echo "DELETE_FILE_ON_TRASHCAN: ${DELETE_FILE_ON_TRASHCAN}"
 export DELETE_FILE_ON_TRASHCAN="${DELETE_FILE_ON_TRASHCAN}"
 
-ls
-pwd
 ./docker-entrypoint.sh
