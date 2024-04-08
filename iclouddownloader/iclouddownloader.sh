461595902492aaa0b80caaf1c4936b9cd7acff74
jdeath <17914369+jdeath@users.noreply.github.com>
2024-04-07 20:32:41 -0400

    Update iclouddownloader.sh

--- iclouddownloader/iclouddownloader.sh
+++ iclouddownloader/iclouddownloader.sh
@@ -9,10 +9,17 @@ password=SecretPassword
 interval=86400
 folderstructure={:%Y/%m}
 
-icloudpd --cookie-directory /data --directory $photoLocation -u $username -p $password --watch-with-interval $interval --folder-structure $folderstructure &
+if [ $# -eq 0 ]
+  then
+    icloudpd --cookie-directory /data --directory $photoLocation -u $username -p $password --watch-with-interval $interval --folder-structure $folderstructure &
+  else
+	icloudpd --cookie-directory /data -u $username -p $password --auth-only
+fi
+# End Account 1
 
 
 # Add another copy above for another account
+You will have to add a & after reach account
 
 # Leave this sleep in. This allows time to log into to docker and run authorization
 echo "If need to authorize, run this command in home assistant SSH shell (need to disable prodcution mode"
