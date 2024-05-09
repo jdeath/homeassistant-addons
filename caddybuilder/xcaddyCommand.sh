1d72a07de41aea0e707bca4e70829e95ea6adcd6
jdeath <17914369+jdeath@users.noreply.github.com>
2024-05-09 06:29:34 -0400

    Update xcaddyCommand.sh
    add latest as default, to avoid compiling a development build

--- caddybuilder/xcaddyCommand.sh
+++ caddybuilder/xcaddyCommand.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xcaddy build --with github.com/caddy-dns/cloudflare --with github.com/caddyserver/transform-encoder --with github.com/mholt/caddy-dynamicdns --with github.com/hslatman/caddy-crowdsec-bouncer
+xcaddy build latest --with github.com/caddy-dns/cloudflare --with github.com/caddyserver/transform-encoder --with github.com/mholt/caddy-dynamicdns --with github.com/hslatman/caddy-crowdsec-bouncer
