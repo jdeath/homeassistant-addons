306d35246e1e8be230f75b120085237671087273
jdeath <17914369+jdeath@users.noreply.github.com>
2024-08-11 12:00:36 -0400

    Update run.sh

--- archivebox/run.sh
+++ archivebox/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-#mkdir -p /data/archivebox
-#cd /data/archivebox
-cd /config/
+mkdir -p /data/archivebox
+cd /data/archivebox
+#cd /config/
 archivebox server --quick-init 0.0.0.0:8000
