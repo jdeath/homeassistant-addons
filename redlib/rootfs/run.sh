8a207f0f86dc003a07b1b99bf84fdc96e45af98e
DDanii <ddani1995@freemail.hu>
2026-05-14 05:14:05 +0200

    redlib config options

---
+++ redlib/rootfs/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bashio
+
+JSONSOURCE="/data/options.json"
+mapfile -t arr < <(jq -r 'keys[]' "${JSONSOURCE}")
+for KEY in "${arr[@]}"; do
+    if bashio::config.has_value "$KEY"; then
+        VALUE=$(bashio::config "$KEY")
+        export "$KEY=$VALUE"
+    fi
+done
+
+exec redlib
